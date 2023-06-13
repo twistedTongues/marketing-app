d091bf63c2a744b488ef37dfa64c1b3ac7574ef5
qwerty-fish <qwertyfish>
2023-06-13 16:12:33 +0300

    feat(app): made homepage
    Done fully responsible homepage, maybe should check for bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,96 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import IntroImage from '../public/intro.jpg'
+import HeroImage from '../public/hero.jpg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main>
+      <section> 
+        <div className="relative">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
               priority
+              src={HeroImage}
+              alt="hero"
+              objectFit="cover"
+              fill
+              className="opacity-20" 
             />
-          </a>
+          <div className="relative py-20 lg:py-60 px-5 lg:px-10 max-w-screen-lg mx-auto">
+            <h1 className="pb-7 text-5xl">Bla Bla Startup Name</h1>
+            <p className="pb-7">Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+            </p>
+            <a href="google.com" className="px-4 py-2 text-2xl uppercase bg-white text-blue-500 rounded-sm">View Link</a>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="bg-blue-500">
+          <div className="flex items-center py-20 lg:py-40 px-5 lg:px-10 max-w-screen-lg mx-auto">
+          <div className="w-1/2 px-5">
+            <h2 className="pb-7 text-3xl font-light">Our product makes your life easier.</h2>
+            <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation ameSit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidat. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
+          </div>
+          <div className="relative h-60 w-1/2">
+            <Image src={IntroImage} alt="intro" fill objectFit="cover"/> 
+          </div>
+          </div>
+      </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section>
+        <div className="max-w-screen-md mx-auto">
+          <ul className="flex justify-around py-20 text-center">
+            <li><span className="text-5xl">15</span><p className="text-sm">Awards Won</p></li>
+            <li><span className="text-5xl">104</span><p className="text-sm">Happy Customers</p></li>
+            <li><span className="text-5xl">0</span><p className="text-sm">Hassle</p></li>
+          </ul>
+          </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section className="bg-blue-500">
+        <div className="py-20 lg:py-60 px-5 lg:px-10 max-w-screen-lg mx-auto">
+          <h3 className="text-4xl lg:text-5xl font-light pb-7">Featured Posts</h3>
+          <div className="pb-7">
+            <ul className="flex flex-col md:flex-row md:justify-between">
+              <li className="pb-7"> 
+                <Link href="/" className="flex">
+                  <div className="relative h-20 w-20">
+                    <Image src={HeroImage} alt="post" fill objectFit="cover"/>
+                  </div>
+                  <div className="pl-5">
+                    <h5 className="text-xl font-semibold">Placeholder</h5>
+                    <p>Apr dd, yyyy</p>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link href="/" className="flex">
+                  <div className="relative h-20 w-20">
+                    <Image src={IntroImage} alt="post" fill objectFit="cover"/>
+                  </div>
+                  <div className="pl-5">
+                    <h5 className="text-xl font-semibold">Placeholder</h5>
+                    <p>Apr dd, yyyy</p>
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex justify-center">
+            <Link href="/blog" className="px-4 py-2 text-xl uppercase bg-white text-blue-500 rounded-sm">View all posts</Link>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section>
+        <div className="py-20 lg:py-40 px-5 lg:px-10 text-center max-w-screen-lg mx-auto">
+          <h4 className="pb-7 text-5xl font-light">Ready to take the next step?</h4>
+          <p className="pb-7">Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
+          <div className="flex justify-center">
+            <Link href="/contact" className="px-4 py-2 text-xl uppercase bg-white text-blue-500 rounded-sm">Contact Us</Link>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
