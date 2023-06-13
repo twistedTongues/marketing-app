6ff31c8dbaf77ce643691b94e9c3b4a412e6eba2
qwerty-fish <qwertyfish>
2023-06-13 16:10:14 +0300

    feat(app): add header and footer

---
+++ app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer  = () => {
+  return (
+    <footer className="p-5 bg-blue-500 flex justify-center items-center lg:h-28">
+      © 2023 StartupName, Inc.
+    </footer>
+  )
+}
+
+export default Footer
