a69b82dddf4481223f12a4c710ddd187dbac34e0
qwerty-fish <qwertyfish>
2023-06-15 12:05:31 +0300

    refactor(app): refactoring design of blog page and posts

--- app/blog/[postId]/page.tsx
+++ app/blog/[postId]/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Post } from '../../../typings'
+import Image from 'next/image'
+import HeroImage from '../../../public/hero.jpg'
+import { notFound } from 'next/navigation'
+
+export const dynamicParams = true; // Default Behaviour
 
 type PageProps = {
   params: {
@@ -8,19 +13,44 @@ type PageProps = {
 }
 
 const fetchPost = async(postId: string) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-  const post = await res.json();
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, { next: { revalidate: 60 } });
+  const post: Post = await res.json();
   return post;
 }
 
 const PostPage = async({ params: { postId } }: PageProps) => {
-  const post: Post[] = await fetchPost(postId);
+  const post = await fetchPost(postId);
+  
+  if(!post.id) return notFound();
+  
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
+    <main>
+      <div className="px-5 max-w-screen-lg mx-auto">
+      <div className="py-20">
+      <h1 className="text-4xl pb-2">{post.title}</h1>
+      <p className="pb-1">By: {post.userId}</p>
+      <p className="text-blue-500">Apr 18, 2020</p>
+      <hr className="h-px my-8 bg-gray-50 border-0 dark:bg-gray-150" />
+      </div>
+      <div className="pb-20">
+      <div className="pb-10">
+        <Image src={HeroImage} alt="post" className="rounded-lg"/>
+      </div>
+      <p className="text-blue-50">{post.body}</p>
+      </div>
+      </div>
+    </main>
   )
 }
 
 export default PostPage
+
+export async function generateStaticParams() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+  const posts: Post[]  = await res.json();
+
+  // For this demo, prebuilding the first 10 pages to avoid being rate limited by the Demo Api
+  const trimmedPosts = posts.splice(0,10);
+  
+  return posts;
+}
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,8 +4,10 @@ import PostsList from './PostsList'
 const Blog = () => {
   return (
     <main>
+      <div className="max-w-screen-lg mx-auto">
       <h1 className="py-20 text-5xl text-center">Blog</h1> 
       <PostsList />
+      </div>
     </main>
   )
 }
