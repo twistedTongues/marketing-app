559781a06fa06f17ecae35c89615cdf6513c1fc8
qwerty-fish <qwertyfish>
2023-06-15 12:04:09 +0300

    refactor(app): refactor design of footer and header

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer  = () => {
   return (
-    <footer className="p-5 bg-blue-500 flex justify-center items-center lg:h-28 drop-shadow-md">
+    <footer className="p-5 bg-blue-500 flex justify-center items-center lg:h-28 drop-shadow-md tracking-widest bottom-0">
       © 2023 StartupName, Inc.
     </footer>
   )
--- app/Header.tsx
+++ app/Header.tsx
@@ -1,13 +1,31 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { motion } from "framer-motion"
 
 const Header = () => {
   return (
     <header className="bg-blue-500">
       <nav className="flex justify-center items-center p-5 lg:h-28">
-        <Link href="/" className="px-2 lg:px-4 py-1 lg:py-2 bg-white text-blue-500 rounded-lg lg:text-3xl drop-shadow-lg">
+        <motion.div
+        initial={{
+            x:-500,
+            opacity: 0,
+            scale: 0.5
+          }}
+        animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1
+          }}
+        transition={{
+          duration: 1,
+        }}
+        >
+        <Link href="/" className="px-2 lg:px-4 py-1 lg:py-2 bg-white text-blue-500 rounded-lg lg:text-3xl drop-shadow-lg tracking-wider">
           Startup Name
         </Link>
+        </motion.div>
       </nav>
     </header>
   )
